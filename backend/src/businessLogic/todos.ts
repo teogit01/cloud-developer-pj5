24f6f595ed8a5bf4c43efda1d88e2e3c7dffc9f0
teogit01 <teob1605272@gmail.com>
2023-06-14 23:53:09 +0700

    fix review 2

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -28,13 +28,13 @@ export const createTodo = async (
 
     const todoId = uuid.v4()
     const createdAt = new Date().toISOString()
-    const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
+    //const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
     const newItem = {
         userId,
         todoId,
         createdAt,
         done: false,
-        attachmentUrl: s3AttachmentUrl,
+        //attachmentUrl: s3AttachmentUrl,
         ...newTodo
     }
     return await todosAccess.createTodoItem(newItem)
